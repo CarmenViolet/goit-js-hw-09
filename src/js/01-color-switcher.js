02ccbe9a82bcc47285c8490ea5d86dcc1f070b12
CarmenViolet <margaretholmes200@gmail.com>
2022-09-07 15:39:10 +0300

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]')
+const bodyEl = document.querySelector('body');
+
+
+buttonStart.addEventListener('click', addRandomColorOn);
+buttonStop.addEventListener('click', stopRandomColor);
+let timerId = null;
+
+function addRandomColorOn() {
+    timerId = setInterval(() => {
+  return bodyEl.style.background = getRandomHexColor()
+   }, 1000)
+
+   buttonStart.disabled = true;
+}
+
+
+function stopRandomColor() {
+    clearInterval(timerId);
+
+    buttonStart.disabled = false;
+}
